352c5ceeb0192079288b250c647e817a3be172f4
Shohan <shohan26579@gmail.com>
2026-08-26 22:41:03 +0200

    Add offline-first service worker

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "unideb-shell-v1";
+const CORE = ["./", "./index.html", "./styles.css", "./app-enhancements.css", "./script.js", "./UniLogo.png", "./Background.jpg", "./manifest.webmanifest"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
+self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match("./index.html"))));
+});
